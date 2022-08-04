b4fbf014e69ea9a89b120e638baf08160cdbf15d
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-03 21:41:45 -0600

    adding in int and more arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max){
+  return Math.floor(Math.random()*(max - min) + min)
+} //random integer
+
 function generatePassword(){
 
-  var userInput = window.prompt("How long would you like your password to be?") 
-  var passLength = parseInt(userInput)
+  var userInput = window.prompt("How long would you like your password to be? (8-128 characters)") 
+  var passLength = parseInt(userInput) //Creator Note: parseInt tries to turn a string into number
 
   if(isNaN(passLength)) {
     window.alert("You did not enter a number, Please Try Again ! ")
@@ -23,8 +27,42 @@ function generatePassword(){
   var userRequestsUpperCaseLet = window.confirm("Do you want Upper Case Letters in your password? (ex. A,B,C) ")
   var userRequestsLowerCaseLet = window.confirm("Do you want Lower Case Letters in your password? (ex. a,b,c) ")
 
+  var speciaCharList = [ "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "~", "[", "{", "]", "}", ";", "'", ":", ",", "<", ">", ".", "/", "?"];
+  var numberList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var lowerCaseLet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCaseLet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  
+  // pushing the List
+  var symbolsCart = []
+  for (var i  = 0; i < lowerCaseLet.length; i++){
+    upperCaseLet[i] = lowerCaseLet[i].toUpperCase()
+
+  } 
+  if (userRequestsNumber === true){
+  symbolsCart.push(numberList)
+  }
+  if (userRequestsSpecialChar === true){
+    symbolsCart.push(speciaCharList)
+  }
+  if (userRequestsUpperCaseLet === true){
+      symbolsCart.push(upperCaseLet)
+  }
+  if (userRequestsLowerCaseLet === true){
+    symbolsCart.push(lowerCaseLet)
+    }
+    
+    console.log(symbolsCart)
+
+
+    var generatePassword = ""
+
+    for (var i = 0; i < passLength; i++){
+       var randomItem = symbolsCart[randomInt(0, symbolsCart.length - 1)]
+    }
+
+
 }
-//Creator Note: parseInt tries to turn a string into number
+
 
 
 
