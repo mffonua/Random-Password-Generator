ee2ee201e5c9282cc1fce09f4a6ae1dc191c6445
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-03 18:26:26 -0600

    added in pop up and character lenght

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
 
-  window.prompt("Hello Bicc BOii")
+  var userInput = window.prompt("How long would you like your password to be?") 
+  var passLength = parseInt(userInput)
+
+  if(isNaN(passLength)) {
+    window.alert("You did not enter a number, Please Try Again ! ")
+    return 
+  }
+  // Creator Note : Reason why the two "if" statements were not combined because
+  // if they are false I wouldnt know which one is.
+
+  if (passLength < 8 || passLength > 128){
+    window.alert("Your password length must be between 8-128 Characters")
+    return
+  }
+
 }
+//Creator Note: parseInt tries to turn a string into number
+
+
 
 // Write password to the #password input
 function writePassword() {
