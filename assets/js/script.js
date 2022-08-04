8116ef12ab46887ca71cd682762f121d15aea824
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-03 23:05:24 -0600

    added in final code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,65 +2,100 @@
 var generateBtn = document.querySelector("#generate");
 
 function randomInt(min, max){
-  return Math.floor(Math.random()*(max - min) + min)
-} //random integer
+  if (!max){
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+    return Math.floor(min*(1 - rand) + rand*max)
+  }
+
+  function getRandomItem(list) {
+    return list[randomInt(0, list.length -1)]
+  }
 
 function generatePassword(){
 
-  var userInput = window.prompt("How long would you like your password to be? (8-128 characters)") 
-  var passLength = parseInt(userInput) //Creator Note: parseInt tries to turn a string into number
+  // var userInput = window.prompt("How long would you like your password to be? (8-128 characters)") 
+  // var passLength = parseInt(userInput) //Creator Note: parseInt tries to turn a string into number
 
-  if(isNaN(passLength)) {
-    window.alert("You did not enter a number, Please Try Again ! ")
-    return 
-  }
-  // Creator Note : Reason why the two "if" statements were not combined because
-  // if they are false I wouldnt know which one is.
+  // if(isNaN(passLength)) {
+  //   window.alert("You did not enter a number, Please Try Again ! ")
+  //   return 
+  // }
+  // // Creator Note : Reason why the two "if" statements were not combined because
+  // // if they are false I wouldnt know which one is.
 
-  if (passLength < 8 || passLength > 128){
-    window.alert("Your password length must be between 8-128 Characters")
-    return
+  // if (passLength < 8 || passLength > 128){
+  //   window.alert("Your password length must be between 8-128 Characters")
+  //   return
+  // }
+
+
+//Loop
+  while (true){
+      var userInput = window.prompt("How long would you like your password to be? (8-128 characters)") 
+      
+      //if User exits prompt
+      if (userInput === null){
+        return
+      }
+
+      var passLength = parseInt(userInput) //Creator Note: parseInt tries to turn a string into number
+
+    if(isNaN(passLength)) {
+      window.alert("You did not enter a number, Please Try Again ! ")  
+    } else if (passLength < 8 || passLength > 128) {
+      window.alert("Invalid password Length. Must be between 8 and 128 Characters. ")
+    } else {
+      break // Creator Note : break means to break out current loop.
+    }
+
   }
+
 //Prompts for Number, Special Characters, UpperCase Letters and LowerCase Letter.
   var userRequestsNumber = window.confirm("Do you want Numbers in your password? (ex. 1,2,3) ")
   var userRequestsSpecialChar = window.confirm("Do you want Special Characters in your password? (ex. !,@,$) ")
   var userRequestsUpperCaseLet = window.confirm("Do you want Upper Case Letters in your password? (ex. A,B,C) ")
   var userRequestsLowerCaseLet = window.confirm("Do you want Lower Case Letters in your password? (ex. a,b,c) ")
 
-  var speciaCharList = [ "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "~", "[", "{", "]", "}", ";", "'", ":", ",", "<", ">", ".", "/", "?"];
-  var numberList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  var lowerCaseLet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCaseLet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var speciaCharList = [ "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "~", "[", "{", "]", "}", ";", "'", ":", ",", "<", ">", ".", "/", "?"]
+  var numberList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList = []
   
   // pushing the List
-  var symbolsCart = []
-  for (var i  = 0; i < lowerCaseLet.length; i++){
-    upperCaseLet[i] = lowerCaseLet[i].toUpperCase()
+  var optionsCart = []
+  
+  for (var i  = 0; i < lowercaseList.length; i++) {
+    uppercaseList[i] = lowercaseList[i].toUpperCase()
 
   } 
-  if (userRequestsNumber === true){
-  symbolsCart.push(numberList)
-  }
-  if (userRequestsSpecialChar === true){
-    symbolsCart.push(speciaCharList)
-  }
-  if (userRequestsUpperCaseLet === true){
-      symbolsCart.push(upperCaseLet)
-  }
-  if (userRequestsLowerCaseLet === true){
-    symbolsCart.push(lowerCaseLet)
-    }
-    
-    console.log(symbolsCart)
-
-
-    var generatePassword = ""
+    if (userRequestsNumber === true) {
+        optionsCart.push(numberList)
+        }
+    if (userRequestsSpecialChar === true) {
+        optionsCart.push(speciaCharList)
+        }
+    if (userRequestsUpperCaseLet === true) {
+        optionsCart.push(uppercaseList)
+        }
+    if (userRequestsLowerCaseLet === true) {
+        optionsCart.push(lowercaseList)
+        }
+    if (optionsCart.length === 0) {
+        optionsCart.push(lowercaseList)
+        }
+  
+    var generatedPassword = ""
 
     for (var i = 0; i < passLength; i++){
-       var randomItem = symbolsCart[randomInt(0, symbolsCart.length - 1)]
+       var randomList = getRandomItem(optionsCart)// Reason -1 is because array starts at 0
+       var randomChar= getRandomItem(randomList)
+       generatedPassword += randomChar
     }
-
-
+    
+    return generatedPassword
 }
 
 
@@ -70,8 +105,12 @@ function generatePassword(){
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  if(!password) {
+    return
+  }
+  if(password){
   passwordText.value = password;
+  }
 
 }
 
